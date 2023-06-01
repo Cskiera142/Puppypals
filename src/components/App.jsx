5bf9b922666c2c1f0ad8a0203874a7b6ddac5ac3
Colton Skiera <cskiera142@gmail.com>
2023-06-01 09:50:53 -0600

    select puppy feature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,36 @@ import { puppyList } from "../data";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick() {
+    // some logic here
+  }
+  let featuredPup = puppies.find((pup) => pup.id === featPupId);
+
   return (
-    <div>
+    <div className="App">
       {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
       })}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
